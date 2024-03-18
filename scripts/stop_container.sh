62eea620419cc619d4d14e8ab533d27c17f1481e
mukeshr-29 <mukeshr2911@gmail.com>
2024-03-18 16:09:02 +0530

    stop script update

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -4,5 +4,5 @@ docker ps
 echo "check whether the container is running"
 container_id=$(docker ps | awk 'NR > 1 {print $1}')
 echo "remove container"
-docker rm "$container_id"
+docker rm -f "$container_id"
 
