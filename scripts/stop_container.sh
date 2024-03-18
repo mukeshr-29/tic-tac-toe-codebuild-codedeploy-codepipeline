8a25aa74a6f2652f71f3478865739e3d966b5bea
mukeshr-29 <mukeshr2911@gmail.com>
2024-03-18 16:50:31 +0530

    stop script update

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,15 +2,4 @@
 
 set -e
 
-echo "Listing Docker containers:"
-docker ps
-
-echo "Checking whether the container is running..."
-container_id=$(docker ps --format "{{.ID}}" | head -n 1)
-
-if [ -z "$container_id" ]; then
-    echo "No running containers found."
-else
-    echo "Removing container with ID: $container_id"
-    docker rm "$container_id"
-fi
+echo "hii"
